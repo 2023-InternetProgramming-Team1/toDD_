0276edee300a69e679857052cbfe15e439cb3e48
tresorhi <tresorhi@naver.com>
2023-12-05 18:53:05 +0900

    [12/5] 이클래스 위치 변경

--- eclass/views.py
+++ eclass/views.py
@@ -4,16 +4,16 @@ from .forms import AssignmentForm, QuizForm
 
 def lecture_list(request):
     lectures = Lecture.objects.all()
-    return render(request, 'lecture_list.html', {'lectures': lectures})
+    return render(request, 'eclass/lecture_list.html', {'lectures': lectures})
 
 def lecture_detail(request, lecture_id):
     lecture = Lecture.objects.get(pk=lecture_id)
     activities = Activity.objects.filter(lecture=lecture)
-    return render(request, 'lecture_detail.html', {'lecture': lecture, 'activities': activities})
+    return render(request, 'eclass/lecture_detail.html', {'lecture': lecture, 'activities': activities})
 
 def activity_detail(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
-    return render(request, 'activity_detail.html', {'activity': activity})
+    return render(request, 'eclass/activity_detail.html', {'activity': activity})
 
 def assignment_creation(request, activity_id):
     activity = Activity.objects.get(pk=activity_id)
@@ -27,11 +27,11 @@ def assignment_creation(request, activity_id):
             assignment.due_date = form.cleaned_data['due_date']
             assignment.activity = activity
             assignment.save()
-            return redirect('lecture_detail', lecture_id=activity.lecture.id)
+            return redirect('eclass/lecture_detail', lecture_id=activity.lecture.id)
     else:
         form = AssignmentForm()
 
-    return render(request, 'assignment_creation.html', {'form': form, 'activity': activity})
+    return render(request, 'eclass/assignment_creation.html', {'form': form, 'activity': activity})
 
 
 def quiz_creation(request, activity_id):
@@ -46,8 +46,8 @@ def quiz_creation(request, activity_id):
             quiz.due_date = form.cleaned_data['due_date']
             quiz.activity = activity
             quiz.save()
-            return redirect('lecture_detail', lecture_id=activity.lecture.id)
+            return redirect('eclass/lecture_detail', lecture_id=activity.lecture.id)
     else:
         form = QuizForm()
 
-    return render(request, 'quiz_creation.html', {'form': form, 'activity':activity})
+    return render(request, 'eclass/quiz_creation.html', {'form': form, 'activity':activity})
